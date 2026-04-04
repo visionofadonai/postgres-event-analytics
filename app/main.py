781253a063281173cad1ad96cdff184e5376c04e
Ryan Cramer <visionofadonai@gmail.com>
2026-04-04 14:16:26 +0000

    Initialize pool at startup

--- app/main.py
+++ app/main.py
@@ -4,11 +4,18 @@
 #print("Current Working Directory:", os.getcwd())
 #print("Python Path:", sys.path)
 from fastapi import FastAPI
-from app.api import events
+#from app.api import events
+from app.async_db import init_db
 
 
-app = FastAPI(title="Event Analytics API")
-app.include_router(events.router)
-@app.get("/health")
-def health():
-    return {"status": "ok"}
+#app = FastAPI(title="Event Analytics API")
+#app.include_router(events.router)
+#@app.get("/health")
+#def health():
+#    return {"status": "ok"}
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await init_db()
