6d022a9c080631d080cc9ecc46a15a4f3eca12db
Ryan Cramer <visionofadonai@gmail.com>
2026-04-20 20:46:19 +0000

    Add Request ID for debugging accross logs

--- app/middleware.py
+++ app/middleware.py
@@ -1,17 +1,25 @@
 import time
 import logging
+import uuid
 from fastapi import Request
 
 logger = logging.getLogger(__name__)
 
+
+
 async def log_requests(request: Request, call_next):
     start_time = time.time()
 
-    logger.info(f"request_start path={request.url.path}")
+    request_id = str(uuid.uuid4())
+
+    logger.info(f"request_start id={request_id} path={request.url.path}")
 
     response = await call_next(request)
 
+    response.headers["X-Request-ID"] = request_id
+
     duration = time.time() - start_time
+
 
     logger.info(
         f"request_end path={request.url.path} status={response.status_code} duration={duration:.4f}s"
